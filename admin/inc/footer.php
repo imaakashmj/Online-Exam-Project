12ab549833653a7178df19120ff5b47e8b812c3a
Aakash Jadhav <aakashkumar.jadhav@gmail.com>
2022-04-04 22:12:49 +0530

    Online-Exam-Project

---
+++ admin/inc/footer.php
@@ -0,0 +1,7 @@
+ </section>
+<section class="footeroption">
+		<h2><?php echo "Made with &#9829 by AJ"; ?></h2>
+	</section>
+</div>
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php 
+    $filepath = realpath(dirname(__FILE__));
+	include_once ($filepath.'/inc/header.php');
+?>
+<style>
+.adminpanel{width: 500px;color: #999;margin: 30px auto 0;padding: 50px;border: 1px solid #ddd;}	
+
+</style>
+
+<div class="main">
+<h1>Admin Panel</h1>
+
+<div class="adminpanel">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQvh05DbbPG8ZGnk0maUJMHV_1ZIPKzh2jnMA&usqp=CAU"/>
+	<h2>Welcome To Control Admin Panel</h2>
+	<p>You can manage your user and online exam system from here....</p>
+
+</div>
+
+	
+</div>
+<?php include 'inc/footer.php'; ?>
